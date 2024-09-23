3a8b48f35f1d72f37b6bb4ca72302bbba8126b5c
vhondos <valentino15@gmail.com>
2024-09-23 20:58:20 +0200

    avance

--- ap10/app.js
+++ ap10/app.js
@@ -1,2 +1,13 @@
 // Ejercicio 10. Encontrar el número más grande en un array
 // Crea una función que encuentre el número más grande en un array.
+
+listaNumeros = Array.from({length: 100}, (_, index) => (Math.random() * Math.random())*10)
+
+aux = 0
+for(item in listaNumeros){
+    if(item > aux){
+        aux = item
+    }
+}
+
+console.log("El número más grande es ",aux)
